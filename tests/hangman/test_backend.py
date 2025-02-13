7dc4a6489fe9d084b4c2b8a04265d111d7829876
Alexey Melnikov <melnikov1991@bk.ru>
2025-02-13 18:03:30 +0300

    add tests for backend

--- tests/hangman/test_backend.py
+++ tests/hangman/test_backend.py
@@ -2,35 +2,45 @@ from hangman import GameStatus, HangmanBackend, State
 
 
 def test_right_answers():
-    hangman_backend = HangmanBackend(["корова", "дерево"])
-
+    hangman_backend = HangmanBackend(["дерево"])
+    selected_word = "дерево"
     hangman_backend.set_state(
         State(
             game_status=GameStatus.IN_PROGRESS,
             wrong_letters_count=0,
-            chosen_word="корова",
-            word_lst=list("корова"),
+            chosen_word=selected_word,
+            word_lst=list(selected_word),
         )
     )
 
-    assert hangman_backend.guess("к")
+    for letter in selected_word:
+        hangman_backend.guess(letter)
 
     state = hangman_backend.get_state()
-
-    assert state.word_lst == ["+", "о", "р", "о", "в", "а"]
-
-    assert hangman_backend.guess("о")
-
-    state = hangman_backend.get_state()
-
-    assert state.word_lst == ["+", "+", "р", "+", "в", "а"]
-
-    assert hangman_backend.guess("р")
-    assert hangman_backend.guess("в")
-    assert hangman_backend.guess("а")
-
-    state = hangman_backend.get_state()
-
-    assert state.word_lst == ["+", "+", "+", "+", "+", "+"]
 
     assert state.game_status == GameStatus.WIN
+    assert state.wrong_letters_count == 1
+    assert state.word_lst == ["+", "+", "+", "+", "+", "+"]
+
+
+def test_wrong_anserts():
+    hangman_backend = HangmanBackend(["дерево"])
+
+    selected_word = "дерево"
+    hangman_backend.set_state(
+        State(
+            game_status=GameStatus.IN_PROGRESS,
+            wrong_letters_count=0,
+            chosen_word=selected_word,
+            word_lst=list(selected_word),
+        )
+    )
+
+    for _ in selected_word:
+        hangman_backend.guess("a")
+
+    state = hangman_backend.get_state()
+
+    assert state.game_status == GameStatus.LOSE
+    assert state.wrong_letters_count == len(selected_word)
+    assert state.word_lst == ["д", "е", "р", "е", "в", "о"]
