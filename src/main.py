c719cbe95249e08f350e0352b74435b1f9ee4c09
Alexey Melnikov <melnikov1991@bk.ru>
2025-02-13 18:00:25 +0300

    change main

--- src/main.py
+++ src/main.py
@@ -3,6 +3,6 @@ from hangman import Hangman, HangmanBackend, HangmanFrontend, TextFileReader
 if __name__ == "__main__":
     words = TextFileReader.read("data/words_case.txt")
 
-    app = Hangman(frontend=HangmanFrontend(), backend=HangmanBackend(), words=words)
+    app = Hangman(frontend=HangmanFrontend(), backend=HangmanBackend(words))
 
     app.start_loop()
