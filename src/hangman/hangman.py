cf9d1ab5e18fa97c2b758444bae2139956ebb05b
alex <melnikov1991@bk.ru>
2025-02-16 19:22:56 +0300

    feat: rebuid main loop

--- src/hangman/hangman.py
+++ src/hangman/hangman.py
@@ -5,37 +5,94 @@ class Hangman:
     def __init__(self, *, backend, frontend) -> None:
         self._backend = backend
         self._frontend = frontend
-
+        
+        
+        
     def start_loop(self):
-        while True:
-            quit_flag = False
-            if not quit_flag:
-                strart_key = input("n - New Game, q - Quit: ").lower()
-                if strart_key not in ("n", "q"):
-                    continue
-                if strart_key == "q":
-                    break
+        
+        
+        runnning = True
+        
+        self._frontend.show_start_menu()
+
+        while ((key := self._frontend.read_keys_menu()) not in ("n", "q")):
+            ...
+            
+        if key == "n":
+            self._backend.reset()
+            self._frontend.set_state(self._backend.get_state())
+            self._frontend.draw()
+            runnning = True
+
+        if key == "q":
+            self._frontend.stop()
+            runnning = False
+
+
+        while runnning:
+            key = self._frontend.read_keys()
+
+            if key == "n":
                 self._backend.reset()
-                backend_state = self._backend.get_state()
-                self._frontend.set_state(backend_state)
+                self._frontend.set_state(self._backend.get_state())
                 self._frontend.draw()
-            else:
+                continue
+
+            if key == "q":
+                self._frontend.stop()
                 break
 
-            while True:
-                key = input("Enter: ")
-                if key == "q":
-                    quit_flag = True
-                    break
+            self._backend.guess(key)
+            self._frontend.set_state(self._backend.get_state())
+            self._frontend.draw()
+
+            if self._backend.get_state().game_status in (
+                GameStatus.LOSE,
+                GameStatus.WIN,
+            ):
+                while (key := self._frontend.read_keys_menu()) not in ("n", "q"):
+                    ...
+                    
                 if key == "n":
                     self._backend.reset()
-                    backend_state = self._backend.get_state()
-                    self._frontend.set_state(backend_state)
+                    self._frontend.set_state(self._backend.get_state())
                     self._frontend.draw()
-                    continue
-                self._backend.guess(key)
-                backend_state = self._backend.get_state()
-                self._frontend.set_state(backend_state)
-                self._frontend.draw()
-                if backend_state.game_status in (GameStatus.LOSE, GameStatus.WIN):
-                    break
+                if key == "q":
+                    self._frontend.stop()
+                    runnning = False
+                
+        
+
+    # def start_loop(self):
+    #     while True:
+    #         quit_flag = False
+    #         if not quit_flag:
+    #             start_key = input("n - New Game, q - Quit: ").lower()
+    #             if start_key not in ("n", "q"):
+    #                 continue
+    #             if start_key == "q":
+    #                 break
+    #             self._backend.reset()
+    #             backend_state = self._backend.get_state()
+    #             self._frontend.set_state(backend_state)
+    #             self._frontend.draw()
+    #         else:
+    #             break
+
+    #         while True:
+    #             key = input("Enter: ")
+    #             if key == "q":
+    #                 quit_flag = True
+    #                 break
+    #             if key == "n":
+    #                 self._backend.reset()
+    #                 backend_state = self._backend.get_state()
+    #                 self._frontend.set_state(backend_state)
+    #                 self._frontend.draw()
+    #                 continue
+    #             self._backend.guess(key)
+    #             backend_state = self._backend.get_state()
+    #             self._frontend.set_state(backend_state)
+    #             self._frontend.draw()
+    #             if backend_state.game_status in (GameStatus.LOSE, GameStatus.WIN):
+    #                 break
